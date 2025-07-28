582a622489cf9c26c24f6d4579b6e216c2cd48c7
Jaivik Goswami <jaivikgoswami780@gmail.com>
2025-07-28 15:27:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,9 @@ app = FastAPI()
 
 # NSE API settings
 HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-                  "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                  'AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/120.0.0.0 Safari/537.36'
 }
 NSE_API = 'https://www.nseindia.com/api/option-chain-equities?symbol={}'
 
